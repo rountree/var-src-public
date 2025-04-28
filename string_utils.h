8babbca2376ba8a747de539a5823aa21dc78122c
Barry <rountree@llnl.gov>
2025-04-28 12:05:57 -0700

    Cleanup header.

--- string_utils.h
+++ string_utils.h
@@ -2,8 +2,6 @@
 #include <stdint.h>
 uint64_t strtouint64_t( const char *restrict nptr, char **restrict endptr, int base );
 uint32_t strtouint32_t( const char *restrict nptr, char **restrict endptr, int base );
-int64_t strtoint64_t( const char *restrict nptr, char **restrict endptr, int base );
-int32_t strtoint32_t( const char *restrict nptr, char **restrict endptr, int base );
 
 unsigned long long safe_strtoull( const char * restrict s );
 void str2timespec( const char * const restrict s, struct timespec *t );
