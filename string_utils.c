9cdb2d8e41f0aa06eea86dffceb0f2274163c646
Barry <rountree@llnl.gov>
2025-04-28 10:09:18 -0700

    Adds str2timespec().

--- string_utils.c
+++ string_utils.c
@@ -28,3 +28,66 @@ unsigned long long safe_strtoull( const char * restrict s ){
     };
     return x;
 }
+
+void str2timespec( const char * const restrict s, struct timespec *t ){
+
+    char *endptr;
+    time_t val;
+
+    assert( sizeof( time_t ) == sizeof( unsigned long long ) );
+
+    errno = 0;
+    val = strtoull( s, &endptr, 0 );
+    if( ERANGE == errno ){
+        printf("%s:%d:%s String passed to str2timespec results in an out-of-range value (%s).  Bye!\n",
+                __FILE__, __LINE__, __func__, s);
+        exit(-1);
+    }
+
+    if( endptr == s ){
+        // Handle null string / non-numeric string case
+        printf( "%s:%d:%s String with no numeric data passed to str2timespec (%s).  Bye!\n",
+                __FILE__, __LINE__, __func__, s );
+        exit(-1);
+    }else if( '\0' == *endptr ){
+        // No time units provided, assume seconds.
+        t->tv_sec  = val;
+        t->tv_nsec = 0;
+    }else{// parse the units.
+        if( 0 == strncmp( endptr, "ns", strlen("ns") ) ){
+            // nanoseconds (ns)
+            t->tv_sec  = val / 1'000'000'000ULL;
+            t->tv_nsec = val % 1'000'000'000ULL;
+        }else if( 0 == strncmp( endptr, "us", strlen("us") ) ){
+            // microseconds (us)
+            t->tv_sec  = val / 1'000'000ULL;
+            t->tv_nsec = (val % 1'000'000ULL) * 1'000ULL;
+        }else if( 0 == strncmp( endptr, "ms", strlen("ms") ) ){
+            // milliseconds (ms)
+            t->tv_sec  = val / 1'000ULL;
+            t->tv_nsec = (val % 1'000ULL) * 1'000'000ULL;
+        }else if( 0 == strncmp( endptr, "s", strlen("s") ) ){
+            // seconds (s)
+            t->tv_sec  = val;
+            t->tv_nsec = 0;
+        }else if( 0 == strncmp( endptr, "m", strlen("m") ) ){
+            // minutes (m), must check after "ms"
+            t->tv_sec  = val * 60;
+            t->tv_nsec = 0;
+        }else if( 0 == strncmp( endptr, "h", strlen("h") ) ){
+            // hours (h)
+            t->tv_sec  = val * 60 * 60;
+            t->tv_nsec = 0;
+        }else if( 0 == strncmp( endptr, "d", strlen("h") ) ){
+            // days (d)
+            t->tv_sec  = val * 60 * 60 * 24;
+            t->tv_nsec = 0;
+        }else{
+            printf( "%s:%d:%s String with unknown units passed to str2timespec (%s).  Bye!\n",
+                    __FILE__, __LINE__, __func__, s );
+            exit(-1);
+        }
+        return;
+    }
+}
+
--- string_utils.h
+++ string_utils.h
@@ -1,3 +1,4 @@
 #pragma once
 
 unsigned long long safe_strtoull( const char * restrict s );
+void str2timespec( const char * const restrict s, struct timespec *t );
